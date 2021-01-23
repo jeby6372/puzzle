366b9a40554b51c0bb05b56bcfc9fbd25bcfe14b
Emmanuel BRUNET <technical@live.fr>
2021-01-23 20:42:16 +0100

    reseting to grid 3X3

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from mapper import Mapper
-from matrix import Matrix, Cell
+from models.exception import PathCompleted, EmptyPath, CellFound
+from models.matrix import Matrix, Cell
+from process import Process
+from runner import Runner
+from scanner import PathScanner
 
+conf_7 = '''7
+1 O;0 ZO;0 Z;0 ZW;0 O;0 W;0 ZW;
+0 O;0 ZO;0 ZO;0 W;0 ZW;0 W;0 Z;
+0 O;0 ZO;0 W;0 Z;0 NW;0 Z;0 ZW;
+0 N;0 O;0 W;0 Z;0 NO;0 N;0 NW;
+0 O;0 O;0 ZW;0 W;0 NW;0 N;0 ZW;
+0 NO;0 O;0 N;0 NW;0 W;0 ZO;0 NW;
+0 O;0 NW;0 NO;0 W;0 N;0 NW;49;
+'''
 conf_5 = '''5
 1 Z;0 O;0 ZO;0 W;0 W;
 0 NO;0 ZO;0 O;0 Z;0 ZW;
@@ -8,31 +20,26 @@ conf_5 = '''5
 17 N;0 NO;0 NW;0 NW;0 N;
 0 O;0 W;0 W;0 NO;25;
 '''
-
 conf_3 = '''3
 1 ZO;0 ZW;0 Z;
 0 Z;0 N;0 N;
 0 O;0 NO;9;'''
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # conf = Array(sys.argv[1])
+    # conf = Array(sys.argv[1:]).join()
+    # with open('input.txt') as f:
+    #   conf = f.read()
     matrix = Matrix(conf_3)
+    scanner = PathScanner(matrix.data)
     print('-> data', matrix.data)
     print('-> vector', matrix.vector)
-    mapper = Mapper(matrix)
 
-    for ri, row in enumerate(matrix.vector):
-        for ci, col in enumerate(row):
-            cell = Cell(ri, ci, matrix.data[ri][ci])
-            # print('check', cell.__dict__)
-            mapper.scan[matrix.vector[ri][ci]](cell)
-            matrix.available = []
+    # Input cell
+    y = 3  # row
+    x = 1  # col
+    ref = Cell(y, x, matrix.data[y][x])
 
-    print(matrix.graph)
-    for slot in matrix.graph:
-        print(slot.__dict__)
-        # for k, v in slot:
-        #     print('cell', k.__dict__)
-        #     for c in v:
-        #         print('available', c.__dict__)
+    result = scanner.scan[matrix.vector[ref.row][ref.col]](ref)
+    for c in result:
+        print(c.__dict__)
+
